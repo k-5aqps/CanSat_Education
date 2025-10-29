9fe1edd0553c3834839bea0dab0a664693ba713a
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-10-29 15:14:55 +0900

    week02_初版

---
+++ examples/01_motor_control/motor.py
@@ -0,0 +1,27 @@
+# 必要なモジュールをimportする
+
+"""
+GPIOの設定をする
+"""
+while True:
+    dir = input("進行方向\n>>>")
+    if dir == "f":
+        """
+        前進
+        """
+    elif dir == "b":
+        """
+        後進
+        """
+    elif dir == "r":
+        """
+        右折
+        """
+    elif dir == "l":
+        """
+        左折
+        """
+    else:
+        """
+        while文を抜ける
+        """
